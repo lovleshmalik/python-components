a1d9acbda290e879bfe62b2b349fea6a720ce48d
lmalik <lmalik@adobe.com>
2022-09-27 10:52:27 +0530

    Merging labmodule02

--- src/main/python/programmingtheiot/cda/system/SystemCpuUtilTask.py
+++ src/main/python/programmingtheiot/cda/system/SystemCpuUtilTask.py
@@ -1,7 +1,7 @@
 #####
-# 
+#
 # This class is part of the Programming the Internet of Things project.
-# 
+#
 # It is provided as a simple shell to guide the student and assist with
 # implementation for the Programming the Internet of Things exercises,
 # and designed to be modified by the student as needed.
@@ -11,16 +11,19 @@ import logging
 import psutil
 
 from programmingtheiot.cda.system.BaseSystemUtilTask import BaseSystemUtilTask
+import programmingtheiot.common.ConfigConst as ConfigConst
+
 
 class SystemCpuUtilTask(BaseSystemUtilTask):
-	"""
-	Shell representation of class for student implementation.
-	
-	"""
+    """
+    Shell representation of class for student implementation.
 
-	def __init__(self):
-		pass
-	
-	def getTelemetryValue(self) -> float:
-		pass
-		
+    """
+
+    def __init__(self):
+        super(SystemCpuUtilTask, self).__init__(
+            name=ConfigConst.CPU_UTIL_NAME, typeID=ConfigConst.CPU_UTIL_TYPE
+        )
+
+    def getTelemetryValue(self) -> float:
+        return psutil.cpu_percent()
